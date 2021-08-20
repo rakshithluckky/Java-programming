aaf248daf6c0b1f49f18a4772b72de8a8628bfdb
rakshithluckky <63341289+rakshithluckky@users.noreply.github.com>
2021-08-20 16:10:41 +0530

    Inheritance type1

---
+++ SingleInheritance.java
@@ -0,0 +1,24 @@
+package singleinheritance;
+
+public class SingleInheritance {
+
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        
+        d.bark();
+        d.eat();
+    }
+    
+}
+
+class Animal{
+    void eat(){
+        System.out.println("Eating");
+    }
+}
+
+class Dog extends Animal{
+    void bark(){
+        System.out.print("Dog is Barking and ");
+    }
+}
