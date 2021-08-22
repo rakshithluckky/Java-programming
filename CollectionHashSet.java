04db41187cefebbf91dff2b68a7ea775b82dc4d6
rakshithluckky <63341289+rakshithluckky@users.noreply.github.com>
2021-08-22 23:17:03 +0530

    Collection Example 2

---
+++ CollectionHashSet.java
@@ -0,0 +1,14 @@
+package collectionhashset;
+import java.util.HashSet;
+
+public class CollectionHashSet {
+    public static void main(String[] args) {
+        HashSet<String> hashset = new HashSet<>();
+        hashset.add("Rocks");
+        hashset.add("Captain");
+        hashset.add("My");
+        hashset.add("My Captain Rocks");
+        System.out.println(hashset);
+    }
+    
+}
